88f956693aa21591d0201f2c67ca3d16b7682205
Mison Caamwang <mison.caamwang@hotmail.com>
2019-08-29 02:59:53 +1000

    added usernames functionality

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let http = require('http').createServer(app);
 let PORT = 3000;
 let io = require('socket.io')(http);
 
+let userNamesArray = [];
+let nickname;
+
+
 //Initalize and create a route
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,11 +19,24 @@ app.get('/', function(req, res){
 app.use('/static', express.static('static'));
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//When client goes on port 3000 on the web browser, the client activates the 'connection' event and this event caught by the server and prints the message.
+//When client goes on port 3000 on the web browser, the client activates the 'connection' event and this event caught by the server and initiates the app.
 //Similar to .addEventListener('eventname', function)  
 io.on('connect', function(socket){
     console.log('a user connected');
     socket.emit('connect');
+
+    //The sever side catches the event named username and the data which is the value inside the input tag id #username.
+    socket.on('username', function(userName){
+      console.log('New User: ' + userName);
+      socket.nickname = userName;
+      
+      userNamesArray.push(userName);
+      console.log(userNamesArray);
+      io.emit('username', userNamesArray);
+      //io.emit('name', data);
+      
+    })
+    
 
     //Disconnect
     socket.on('disconnect', function(){
@@ -28,9 +45,9 @@ io.on('connect', function(socket){
     });
 
     //Prints chat message to the console
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', function(data){
+        console.log(`${socket.nickname}: ` + data);
+        io.emit('chat message', {msg: data, nick: socket.nickname});
     });
 
 });
