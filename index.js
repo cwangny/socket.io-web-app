506b235a8834a3b3249d7dad1136ab53c12b8471
Mison Caamwang <mison.caamwang@hotmail.com>
2019-09-19 14:55:58 +1000

    better comments

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 //Server side
 
+//HIW = How It works.
+
+//Import express.js for setting up the server. 
 let express = require('express');
 let app = express();
 let http = require('http').createServer(app);
+//Heroku assigns a random port so 3000 wont work. 
 let PORT = process.env.PORT || 3000;
 let io = require('socket.io')(http);
-//Global Scope
+//Global scopes variables.
 let userNamesArray = [];
 let nickname;
 
-
-//Initalize and create a route
+//Initalize and create a route using express.
 app.get('/', function(req, res){
   	res.sendFile(__dirname + '/index.html');
 });
@@ -19,15 +22,14 @@ app.get('/', function(req, res){
 app.use('/static', express.static('static'));
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//When client goes on port 3000 on the web browser, the client activates the 'connection' event and this event caught by the server and initiates the app.
+//HIW: When client goes on port 3000 on the web browser, the client activates the 'connection' event and this event is caught by the server and initiates the app.
 //Similar to .addEventListener('eventname', function)  
 io.on('connect', function(socket){
     console.log('a user connected, username not set');
     socket.emit('connect');
 
-    //The sever side catches the event named username and the data which is the value inside the input tag id #username.
-
-    //Add username to userarray function:
+	//Adds users to an array and emmits the new array to the cliet side.
+	//HIW: The sever side catches the event named username and the data which is the value inside the input tag id #username.
     socket.on('username', function(userName){
 		console.log('New User: ' + userName);
 		socket.nickname = userName;
@@ -36,12 +38,9 @@ io.on('connect', function(socket){
 		console.log(userNamesArray);
 		
 		io.emit('updateduserlist', userNamesArray);
-    })
-    
-    //Send Userlist 
+    }) 
 
-    //Removes User from the userlist and returns a new list
-    //Log out functionality
+    //Removes user from the array when log out button is clicked on the client UI. Also reuturns an updated array.
     socket.on('remove user', function(data){
 		console.log('Before Userlist: '+userNamesArray);
 		for (let i = 0; i < userNamesArray.length; i++) {
@@ -55,8 +54,9 @@ io.on('connect', function(socket){
 		console.log('After Userlist: '+ userNamesArray);
 		io.emit('updateduserlist', userNamesArray);
 	})
-    
-    //Disconnect message
+
+    //When a user closes the tab, the disconnect event is fired from the server. 
+    //Removes user from the array when user closes the tab. 
     socket.on('disconnect', function(){
         //io.emit('disconnect', socket.nickname);
 		console.log(socket.nickname +' disconnected');
@@ -73,7 +73,7 @@ io.on('connect', function(socket){
 		io.emit('updateduserlist', userNamesArray);
     });
 
-    //Message function
+    //Receives message and username form the client and returns it. 
     socket.on('chat message', function(data){
         console.log(`${socket.nickname}: ` + data);
         io.emit('chat message', {msg: data, nick: socket.nickname});
@@ -86,6 +86,7 @@ http.listen(PORT, function(){
 });
 
 /*
+App Features:
 - Sign in instantly using a nickname
 - Broadcast a message to connected users when someone connects or disconnects.
 - Add support for nicknames.
